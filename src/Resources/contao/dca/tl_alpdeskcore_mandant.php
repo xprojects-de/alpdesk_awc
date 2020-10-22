cd0111af55e4a381794c29b71be7f163242b808d
XProjects B.Hummel <infos@x-projects.de>
2020-10-22 14:52:43 +0200

    Adding Overrite-Key

--- src/Resources/contao/dca/tl_alpdeskcore_mandant.php
+++ src/Resources/contao/dca/tl_alpdeskcore_mandant.php
@@ -1,9 +1,17 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_alpdeskcore_mandant']['palettes']['default'] = $GLOBALS['TL_DCA']['tl_alpdeskcore_mandant']['palettes']['default'] . ';awckey,awcapikey,awcusername,awcpassword';
+$GLOBALS['TL_DCA']['tl_alpdeskcore_mandant']['palettes']['default'] = $GLOBALS['TL_DCA']['tl_alpdeskcore_mandant']['palettes']['default'] . ';awckey,awckey_overrite,awcapikey,awcusername,awcpassword';
 $GLOBALS['TL_DCA']['tl_alpdeskcore_mandant']['fields']['awckey'] = array
     (
     'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awckey'],
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => array('mandatory' => false, 'maxlength' => 250, 'tl_class' => 'w50'),
+    'sql' => "varchar(250) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_alpdeskcore_mandant']['fields']['awckey_overrite'] = array
+    (
+    'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awckey_overrite'],
     'exclude' => true,
     'inputType' => 'text',
     'eval' => array('mandatory' => false, 'maxlength' => 250, 'tl_class' => 'w50'),
--- src/Resources/contao/languages/de/tl_alpdeskcore_mandant.php
+++ src/Resources/contao/languages/de/tl_alpdeskcore_mandant.php
@@ -1,6 +1,7 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awckey'] = array('AWC-Key', 'Gast-Key für die AWC-Schnittstelle');
+$GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awckey'] = array('AWC-Key', 'Gasthouse-Key für die AWC-Schnittstelle');
+$GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awckey_overrite'] = array('AWC-Key-Overrite', 'Gasthouse-Key-Overrite für die AWC-Schnittstelle');
 $GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awcapikey'] = array('AWC-API-Key', 'API-Key für die AWC-Schnittstelle');
 $GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awcusername'] = array('AWC-Username', 'API-Username für die AWC-Schnittstelle');
 $GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['awcpassword'] = array('AWC-Password', 'API-Password für die AWC-Schnittstelle');
